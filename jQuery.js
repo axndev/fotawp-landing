482f6413d5e05a36c6c5a0038b2c25c3416d210f
Kaleemullah Ahsan <159533186+kaleemullahahsan@users.noreply.github.com>
2024-09-16 11:08:28 +0500

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
     $('#menuClose').click(function(){
         $('#mobile-menu').removeClass('show')
     });
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 50) {  // Change '50' to your preferred scroll distance
+        $('header').css('background-color', 'rgba(0, 0, 0, 0.8)');  // Change to your desired background color
+      } else {
+        $('header').css('background-color', 'transparent');
+      }
+    });
   });
